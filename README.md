2811f62376cd34b8e8b4e662dc10681fcb762c6b
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2022-06-18 15:32:26 +0300

    Make arduino-focuser compatible with Elecrow motor shield
    The arduino-focuser now works with the Elecrow motor shield MCS02930M
    instead of the Pololu motor driver. This is only for my personal Sol'Ex
    focuser project, don't expect this to fully work with any other stepper
    and use case.
    
    https://www.elecrow.com/four-channels-motor-shield-for-arduino-p-310.html

--- README.md
+++ README.md
@@ -1,30 +1,11 @@
 # Arduino-based Telescope Focuser
 
-This sketch implements telescope focuser controller logic for a stepper motor and a Pololu DRV8825 stepper driver board. The serial protocol is compatible with Moonlite focusers, so their ASCOM drivers and supporting software can be used.
+This sketch implements telescope focuser controller logic for a stepper motor and an Elecrow stepper motor driver shield. The serial protocol is compatible with Moonlite focusers, so their ASCOM drivers and supporting software can be used.
 
 Main features:
 
 - Interrupt-driven motor control for fast and accurate operation (up to 1000 steps/s)
-- Microstepping up to 1/32 steps
+- Microstepping
 - Persistent settings and position. Position data is saved to EEPROM after every move
 
-To use this controller, you should first connect your Arduino to the DRV8825 board as described in the Pololu's [DRV8825 product page](https://www.pololu.com/product/2133). If you want to use microstepping or have the driver shut down focuser power when not in use then connect also the remaining pins (ENABLE, M0, M1, M2). Finally, edit `Config.h` to set your pin mapping.
-
-![DRV8825 wiring diagram](https://a.pololu-files.com/picture/0J4232.600.png)
-
 Note: For the Moonlite ASCOM driver to work, you must add a 100uF capacitor to your Arduino between RST and GND to prevent the Arduino from rebooting when the serial connection is opened. Otherwise the ASCOM driver will time out and not detect your controller.
-
-## Differences to the Moonlite controller
-
-The Moonlite driver has the option for selecting the focuser speed from 250 steps/s to 16 steps/s. In order to achieve the high speed of up to 1000 steps/s these values are multiplied by four by the controller.
-
-The driver also has a selection for setting the microstepping mode to "full" or "half". The controller supports 1/1, 1/2, 1/4, 1/8, 1/16 and 1/32 step microstepping modes. Please edit the `Config.h` to select the microstepping modes that the Moonlite "full" and "half" commands should correspond to.
-
-## Focuser hold power
-
-The sketch supports an extension to the Moonlite protocol to enable keeping the focuser continously powered. The hold power can be enabled with the Moonlite Single Channel Controller application that allows you to send arbitrary serial commands to the controller. To enable the hold power send the `HE` command. To disable it, use `HD`. The setting is saved in EEPROM so you need to do this only once.
-
-When the hold power is enabled, the controller keeps the focuser continuously powered. The power is turned off only after no serial traffic is detected for 30 seconds. In this case the ASCOM driver is not connected so we deduce that the controller is not being used.
-
-When the hold power is disabled, the focuser is powered before starting a move and turned off
-slightly after a move has ended.
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Jari Saukkonen, Samuli Vuorinen
+ * Copyright (c) 2022 Jari Saukkonen, Samuli Vuorinen
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
@@ -23,34 +23,28 @@
 
 #include <EEPROM.h>
 #include <TimerOne.h>
-#include "Config.h"
-#include "DRV8825.h"
+#include "AFMotor.h"
 #include "moonlite.h"
 
 #define DRIVER_DISCONNECTED_MILLIS (30 * 1000)
 #define EEPROM_MARKER 'F'
-#define INTERRUPT_RATE_US 500
+#define INTERRUPT_RATE_US 2000
 
 /* Stepper definition */
-DRV8825 stepper(PIN_DIR, PIN_STEP, PIN_ENABLE, PIN_M0, PIN_M1, PIN_M2);
+AF_Stepper stepper(8000, 2);
 
-/* Microstepping level. Original moonlite is full/half, we have different settings from 
-   MICROSTEPS_FULL and MICROSTEPS_HALF. */
 bool useFineMicroSteps = true;
-
-/* Shall we keep focuser power on after the move has ended? */
-bool holdEnabled = false;
 
 /* Timestamp (micros()) of last received serial traffic */
 unsigned long lastSerialReceived = 0;
 
 /* Settings data in EEPROM */
-struct Settings {
+struct Settings
+{
 	uint8_t marker;
 	uint16_t currentPosition;
 	uint8_t useFineMicroSteps;
 	uint8_t delayMultiplier;
-	uint8_t holdEnabled;
 } settings;
 
 /* Serial commands are stored in this buffer for parsing. */
@@ -94,240 +88,254 @@ volatile bool needToSaveSettings = false;
 
 void motorInterrupt();
 
-void loadSettings() {
-	uint8_t* dst = (uint8_t*)&settings;
-	for (unsigned int i = 0; i < sizeof(Settings); i++) {
+void loadSettings()
+{
+	uint8_t *dst = (uint8_t *)&settings;
+	for (unsigned int i = 0; i < sizeof(Settings); i++)
+	{
 		dst[i] = EEPROM.read(i);
 	}
 
-	if (settings.marker == EEPROM_MARKER) {
+	if (settings.marker == EEPROM_MARKER)
+	{
 		currentPosition = settings.currentPosition;
 		delayMultiplier = settings.delayMultiplier;
 		useFineMicroSteps = settings.useFineMicroSteps;
-		holdEnabled = settings.holdEnabled;
-	} else {
+	}
+	else
+	{
 		settings.marker = EEPROM_MARKER;
 		settings.currentPosition = currentPosition = 0;
 		settings.delayMultiplier = delayMultiplier = 2;
 		settings.useFineMicroSteps = useFineMicroSteps = true;
-		settings.holdEnabled = holdEnabled = false;
 	}
 }
 
-void saveSettings() {
+void saveSettings()
+{
 	settings.currentPosition = currentPosition;
 	settings.delayMultiplier = delayMultiplier;
 	settings.useFineMicroSteps = useFineMicroSteps;
-	settings.holdEnabled = holdEnabled;
 
-	uint8_t* dst = (uint8_t*)&settings;
-	for (unsigned int i = 0; i < sizeof(Settings); i++) {
-		if (EEPROM.read(i) != dst[i]) {
+	uint8_t *dst = (uint8_t *)&settings;
+	for (unsigned int i = 0; i < sizeof(Settings); i++)
+	{
+		if (EEPROM.read(i) != dst[i])
+		{
 			EEPROM.write(i, dst[i]);
 		}
 	}
 }
 
-void setup() {
+void setup()
+{
 	loadSettings();
 
 	newPosition = currentPosition;
 	clearBuffer(serialBuffer, 8);
 	Serial.begin(9600);
-	while(!Serial) {
+	while (!Serial)
+	{
 		delay(10);
-	}
-
-	stepper.setMicrostepping(useFineMicroSteps ? MICROSTEPS_HALF : MICROSTEPS_FULL);
-	if (holdEnabled) {
-		stepper.enable();
-	} else {
-		stepper.disable();
 	}
 
 	Timer1.initialize(INTERRUPT_RATE_US);
 	Timer1.attachInterrupt(motorInterrupt);
 }
 
-void disableStepperWithDelay() {
-	disengageCounter = INTERRUPT_RATE_US*2;
+void disableStepperWithDelay()
+{
+	disengageCounter = INTERRUPT_RATE_US * 2;
 }
 
-void handleDelayedDisable() {
-	if (!isMoving && disengageCounter > 0) {
+void handleDelayedDisable()
+{
+	if (!isMoving && disengageCounter > 0)
+	{
 		disengageCounter--;
-		if (disengageCounter == 0 && !holdEnabled) {
-			stepper.disable();
+		if (disengageCounter == 0)
+		{
+			stepper.release();
 		}
-	} else {
+	}
+	else
+	{
 		disengageCounter = 0;
 	}
 }
 
-void motorInterrupt() {
+void motorInterrupt()
+{
 	handleDelayedDisable();
 
 	/* handle only every nth interrupt if a slower speed is selected */
-	if (++interruptCounter < delayMultiplier) {
+	if (++interruptCounter < delayMultiplier)
+	{
 		return;
 	}
 	interruptCounter = 0;
 
-	if (!isMoving) {
+	if (!isMoving)
+	{
 		return;
 	}
 
-	/* Enable stepper if it has been shut down */
-	if (currentPosition != newPosition && !stepper.isEnabled()) {
-		stepper.enable();
-	}
-
 	/* Move stepper and update currentPosition */
-	if (currentPosition < newPosition && currentPosition < 65535) {
-		stepper.step(1);
+	if (currentPosition < newPosition && currentPosition < 65535)
+	{
+		stepper.onestep(FORWARD, useFineMicroSteps ? MICROSTEP : INTERLEAVE);
 		currentPosition++;
-	} else if (currentPosition > newPosition && currentPosition > 0) {
-		stepper.step(-1);
+	}
+	else if (currentPosition > newPosition && currentPosition > 0)
+	{
+		stepper.onestep(BACKWARD, useFineMicroSteps ? MICROSTEP : INTERLEAVE);
 		currentPosition--;
 	}
 
 	/* If we have reached desired position end the move and save new position to EEPROM */
-	if (currentPosition == newPosition) {
+	if (currentPosition == newPosition)
+	{
 		isMoving = false;
 		needToSaveSettings = true;
 		disableStepperWithDelay();
 	}
 }
 
-void handleSerial() {
+void handleSerial()
+{
 	/* Check for serial communications and act accordingly. */
 	if (!Serial.available())
 		return;
 
-	if (Serial.read() == ':') {
+	if (Serial.read() == ':')
+	{
 		clearBuffer(serialBuffer, SERIAL_BUFFER_LENGTH);
 		Serial.readBytesUntil('#', serialBuffer, 8);
 		MOONLITE_COMMAND command = moonliteStringToEnum(serialBuffer);
 		lastSerialReceived = millis();
-		switch (command) {
-			case stop:
-				/* Stop moving */
-				isMoving = false;
-				newPosition = currentPosition;
-				disableStepperWithDelay();
+		switch (command)
+		{
+		case stop:
+			/* Stop moving */
+			isMoving = false;
+			newPosition = currentPosition;
+			disableStepperWithDelay();
+			break;
+		case get_current_position:
+			/* Get current position */
+			char currentPositionString[4];
+			sprintf(currentPositionString, "%04X", currentPosition);
+			Serial.print(currentPositionString);
+			Serial.print("#");
+			break;
+		case set_current_position:
+			/* Set current position */
+			if (isMoving)
 				break;
-			case get_current_position:
-				/* Get current position */
-				char currentPositionString[4];
-				sprintf(currentPositionString, "%04X", currentPosition);
-				Serial.print(currentPositionString);
-				Serial.print("#");
+			currentPosition = four_chars_to_uint16(serialBuffer + 2);
+			saveSettings();
+			break;
+		case get_new_position:
+			/* Get new position set by SN */
+			char newPositionString[4];
+			sprintf(newPositionString, "%04X", newPosition);
+			Serial.print(newPositionString);
+			Serial.print("#");
+			break;
+		case set_new_position:
+			/* Set new position */
+			if (isMoving)
 				break;
-			case set_current_position:
-				/* Set current position */
-				if (isMoving) break;
-				currentPosition = four_chars_to_uint16(serialBuffer + 2);
-				saveSettings();
-				break;
-			case get_new_position:
-				/* Get new position set by SN */
-				char newPositionString[4];
-				sprintf(newPositionString, "%04X", newPosition);
-				Serial.print(newPositionString);
-				Serial.print("#");
-				break;
-			case set_new_position:
-				/* Set new position */
-				if (isMoving) break;
-				newPosition = four_chars_to_uint16(serialBuffer + 2);
-				break;
-			case go_to_new_position:
-				/* Go to new position set by SN */
-				isMoving = true;
-				break;
-			case check_if_half_step:
-				/* Check if half-stepping */
-				if (useFineMicroSteps) {
-					Serial.print("FF#");
-				} else {
-					Serial.print("00#");
-				}
-				break;
-			case set_full_step:
-				/* Set full-step mode */
-				if (isMoving) break;
-				stepper.setMicrostepping(MICROSTEPS_FULL);
-				useFineMicroSteps = false;
-				saveSettings();
-				break;
-			case set_half_step:
-				/* Set half-step mode */
-				if (isMoving) break;
-				stepper.setMicrostepping(MICROSTEPS_HALF);
-				useFineMicroSteps = true;
-				saveSettings();
-				break;
-			case check_if_moving:
-				/* Check if moving */
-				if (isMoving) {
-					Serial.print("01#");
-				} else {
-					Serial.print("00#");
-				}
-				break;
-			case get_firmware_version:
-				/* Get firmware version */
-				Serial.print("01#");
-				break;
-			case get_backlight_value:
-				/* Get current Red LED Backlight value */
+			newPosition = four_chars_to_uint16(serialBuffer + 2);
+			break;
+		case go_to_new_position:
+			/* Go to new position set by SN */
+			isMoving = true;
+			break;
+		case check_if_half_step:
+			/* Check if half-stepping */
+			if (useFineMicroSteps)
+			{
+				Serial.print("FF#");
+			}
+			else
+			{
 				Serial.print("00#");
+			}
+			break;
+		case set_full_step:
+			/* Set full-step mode */
+			if (isMoving)
 				break;
-			case get_speed:
-				/* Get speed */
-				char speedString[2];
-				sprintf(speedString, "%02X", delayMultiplier);
-				Serial.print(speedString);
-				Serial.print("#");
+			useFineMicroSteps = false;
+			saveSettings();
+			break;
+		case set_half_step:
+			/* Set half-step mode */
+			if (isMoving)
 				break;
-			case set_speed:
-				/* Set speed */
-				if (isMoving) break;
-				delayMultiplier = two_chars_to_uint8(serialBuffer + 2);
-				saveSettings();
+			useFineMicroSteps = true;
+			saveSettings();
+			break;
+		case check_if_moving:
+			/* Check if moving */
+			if (isMoving)
+			{
+				Serial.print("01#");
+			}
+			else
+			{
+				Serial.print("00#");
+			}
+			break;
+		case get_firmware_version:
+			/* Get firmware version */
+			Serial.print("01#");
+			break;
+		case get_backlight_value:
+			/* Get current Red LED Backlight value */
+			Serial.print("00#");
+			break;
+		case get_speed:
+			/* Get speed */
+			char speedString[2];
+			sprintf(speedString, "%02X", delayMultiplier);
+			Serial.print(speedString);
+			Serial.print("#");
+			break;
+		case set_speed:
+			/* Set speed */
+			if (isMoving)
 				break;
-			case get_temperature:
-				/* TODO: Get temperature */
-				Serial.print("0000#");
-				break;
-			case set_hold_enabled:
-				holdEnabled = true;
-				stepper.enable();
-				saveSettings();
-				break;
-			case set_hold_disabled:
-				holdEnabled = false;
-				stepper.disable();
-				saveSettings();
-				break;
-			case unrecognized:
-				/* We ignore other commands */
-				break;
-			default:
-				break;
+			delayMultiplier = two_chars_to_uint8(serialBuffer + 2);
+			saveSettings();
+			break;
+		case get_temperature:
+			/* TODO: Get temperature */
+			Serial.print("0000#");
+			break;
+		case unrecognized:
+			/* We ignore other commands */
+			break;
+		default:
+			break;
 		}
 	}
 }
 
-void disableStepperIfNoSerialTraffic() {
+void disableStepperIfNoSerialTraffic()
+{
 	/* timeout focuser power if no data is received for a while; driver is most likely disconnected */
-	if (stepper.isEnabled() && (millis() - lastSerialReceived > DRIVER_DISCONNECTED_MILLIS)) {
-		stepper.disable();
+	if ((millis() - lastSerialReceived > DRIVER_DISCONNECTED_MILLIS))
+	{
+		stepper.release();
 	}
 }
 
-void loop() {
-	if (needToSaveSettings) {
+void loop()
+{
+	if (needToSaveSettings)
+	{
 		saveSettings();
 		needToSaveSettings = false;
 	}
